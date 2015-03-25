4cff11ea94dcc56a716679bcc2b35dd064287a0c
Juan Morales <juan.morales@upm.es>
2015-03-25 17:22:01 +0100

    snapshot

--- chrono.jsx
+++ chrono.jsx
@@ -4,10 +4,15 @@ var Chrono = React.createClass({
 	    <div className="chrono">
 	      <Clock countdown={this.props.countdown} />
 	      <div className="controls">
-		<input type="number" defaultValue="7" min="0"/> days. In
-		<input type="number" defaultValue="20" min="0"/> minutes. 							  
-                <button>Start</button>
+		<input ref="fakeInput" type="number" defaultValue="7" min="0"/> 
+		days. In
+		<input ref="countdownInput" type="number" defaultValue="20" min="0"/> 
+		minutes. 
+                <button onClick={setClock}>Set</button>
 	      </div>
 	    </div>);
+    },
+    setClock: function() {
+	this.refs.fakeInput.value
     }
 });
