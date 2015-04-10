1d4dc0ecc0d92b0ac4bb9eacc930c1544887c6c8
Juan Morales <crispamares@gmail.com>
2015-04-10 02:48:07 +0200

    real final seconds

--- chrono.jsx
+++ chrono.jsx
@@ -5,14 +5,17 @@ var Chrono = React.createClass({
 	    isRunning: false,
 	    realCountdownStep: null,
 	    fakeCountdownStep: null,
+	    threshold: null,  
 	    intervalId: null
 	};
     },
     setClock: function() {
 	
 	var days = parseInt(React.findDOMNode(this.refs.fakeInput).value);
-	var minutes = parseInt(React.findDOMNode(this.refs.countdownInput).value)
+	var minutes = parseInt(React.findDOMNode(this.refs.countdownInput).value);
+	var realSeconds = parseInt(React.findDOMNode(this.refs.countdownThreshold).value);
 
+	var threshold = moment.duration(realSeconds, 'seconds');
 	var realCountdown = moment.duration(minutes, 'minutes').asMilliseconds();
 	var fakeCountdowm = moment.duration(days, 'days').asMilliseconds();
 
@@ -20,8 +23,13 @@ var Chrono = React.createClass({
 	this.state.fakeCountdownStep = fakeCountdowm * this.state.realCountdownStep  / realCountdown;
 
 	this.state.countdown = fakeCountdowm;
+	this.state.threshold = threshold;
 
 	this.setState(self.state);
+    },
+    _inHurry: function() {
+	return ((this.state.countdown / this.state.fakeCountdownStep) * this.state.realCountdownStep)  
+	             < this.state.threshold;
     },
     play: function () {
 	if (this.state.isRunning) {
@@ -32,7 +40,12 @@ var Chrono = React.createClass({
 	    var self = this;
 	    this.state.isRunning = true;
 	    this.state.intervalId = setInterval(function() {
-		self.state.countdown = self.state.countdown - self.state.fakeCountdownStep;
+		var step = self.state.fakeCountdownStep; 
+		if (self._inHurry()) {
+		    step =  50;
+		    self.state.countdown = Math.min(self.state.countdown, self.state.threshold);
+		}
+		self.state.countdown = self.state.countdown - step;
 		self.setState(self.state);
 	    }, this.state.realCountdownStep);
 	}
@@ -40,7 +53,8 @@ var Chrono = React.createClass({
     },
     render: function() {
 	var playMsg = (this.state.isRunning) ? "Pause" : "Play";
-	var hurry = ((this.state.countdown / this.state.fakeCountdownStep) * this.state.realCountdownStep)  < moment.duration(10, 'seconds');
+	var threshold = this.state.threshold;
+	var hurry = this._inHurry();
 	console.log(((this.state.countdown / this.state.fakeCountdownStep) * this.state.realCountdownStep));
 	return (
 	    <div className="chrono">
@@ -49,7 +63,9 @@ var Chrono = React.createClass({
 		<input ref="fakeInput" type="number" defaultValue="7" min="0"/> 
 		days. In
 		<input ref="countdownInput" type="number" defaultValue="20" min="0"/> 
-		minutes. 
+		minutes. The last 
+		<input ref="countdownThreshold" type="number" defaultValue="60" min="0"/> 
+		seconds are real. 
                 <button onClick={this.setClock}>Set</button>
                 <button onClick={this.play}>{playMsg}</button>
 	      </div>
